2fecdcd70560b147f499b006ea3772b78296af39
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-02 11:23:53 -0400

    Added new MapView component for save screens

--- src/Map/MapDisplayWrapper.js
+++ src/Map/MapDisplayWrapper.js
@@ -3,6 +3,7 @@ import React, {useContext} from "react";
 import DistanceContext from "../Context/DistanceContext";
 import UnitContext from "../Context/UnitContext";
 import MarkersContext from "../Context/MarkersContext";
+import RouteContext from "../Context/RouteContext";
 
 import MapDisplay from "./MapDisplay";
 
@@ -10,8 +11,9 @@ function MapDisplayWrapper(props) {
   const unit = useContext(UnitContext);
   const distance = useContext(DistanceContext);
   const markers = useContext(MarkersContext);
+  const route = useContext(RouteContext);
   return (
-    <MapDisplay unit={unit} distance={distance} markers={markers} navigation={props.navigation} />
+    <MapDisplay unit={unit} distance={distance} markers={markers} route={route} navigation={props.navigation} />
   )
 }
 

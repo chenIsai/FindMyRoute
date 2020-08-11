e7a816bb316c885a95b81fff134e3562d8295395
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-11 00:15:06 -0400

    Added image assets

---
+++ src/Images/index.js
@@ -0,0 +1,9 @@
+module.exports.logo = require("./FindMyRouteOriginal.png");
+module.exports.logo300 = require("./FindMyRoute300.png");
+module.exports.logo300 = require("./FindMyRoute600.png");
+module.exports.logo300 = require("./FindMyRoute900.png");
+
+// export const logo = require("./FindMyRouteOriginal.png");
+// export const logo300 = require("./FindMyRoute300.png");
+// export const logo600 = require("./FindMyRoute300.png");
+// export const logo900 = require("./FindMyRoute900.png");

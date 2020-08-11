0724ee7721250e9afcfcbeff4ea04f101ed5c337
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-11 00:37:45 -0400

    Fixed issues with white space

--- src/Images/index.js
+++ src/Images/index.js
@@ -1,9 +1,7 @@
-module.exports.logo = require("./FindMyRouteOriginal.png");
 module.exports.logo300 = require("./FindMyRoute300.png");
-module.exports.logo300 = require("./FindMyRoute600.png");
-module.exports.logo300 = require("./FindMyRoute900.png");
+module.exports.logo600 = require("./FindMyRoute600.png");
+module.exports.logo900 = require("./FindMyRoute900.png");
 
-// export const logo = require("./FindMyRouteOriginal.png");
 // export const logo300 = require("./FindMyRoute300.png");
 // export const logo600 = require("./FindMyRoute300.png");
 // export const logo900 = require("./FindMyRoute900.png");

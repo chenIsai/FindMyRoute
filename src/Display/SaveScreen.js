7887944426fe72bc2c2ffbcc946a3eb33cb8086a
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-15 01:46:21 -0400

    Moved savescreen to proper folder

--- src/Display/SaveScreen.js
+++ src/Display/SaveScreen.js
@@ -8,7 +8,8 @@ import RouteContext from "../Context/RouteContext";
 
 import AsyncStorage from "@react-native-community/async-storage";
 
-import LiteView from "./LiteView";
+import LiteMap from "./LiteMap";
+import Icon from "react-native-vector-icons/Ionicons"
 
 function SaveScreen({navigation}) {
   const [name, _onChangeName] = useState("");
@@ -22,21 +23,30 @@ function SaveScreen({navigation}) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.emptySpace} />
-      <View style={styles.detailsView}>
-        <LiteView markers={markers.value} directions={directions.value} distance={showDistance} unit={unit.value} name={name} description={description}/>
+      <View style={styles.mapStyle}>
+        <LiteMap markers={markers.value} directions={directions.value}/>
       </View>
-      <View style={styles.saveOptions}>
+      <View>
         <View style={styles.nameRow}>
-          <Text>Icon</Text>
+          <Icon
+            style={{paddingRight: 5}}
+            name={"bookmark"}
+            color={"#b0b2f5"}
+            size={30}
+            />
           <TextInput style={styles.nameInput}
             maxLength={36}
-            onChangeText={text => _onChangeName(text)}
+            onChangeText={ text => _onChangeName(text)}
             placeholder={"Name"}
           />
         </View>
         <View style={styles.descriptionRow}>
-          <Text>Icon</Text>
+          <Icon
+            style={{paddingRight: 5, alignSelf: "center"}}
+            name={"document-text"}
+            color={"#b0b2f5"}
+            size={30}
+            />
           <TextInput style={styles.descriptionInput}
             maxLength={160}
             multiline={true}
@@ -45,34 +55,38 @@ function SaveScreen({navigation}) {
             onChangeText={text => _onChangeDesc(text)}
             placeholder={"Enter a description for the route (optional)"} />
         </View>
-        <View style={styles.buttonView}>
-        <TouchableNativeFeedback
-            background={TouchableNativeFeedback.Ripple("#AAF", true)}
-            style={styles.submitButton}
-            onPress={() =>{
-              if (!name) {
-                Alert.alert("Please enter a name for your route!");
-                return;
-              }
-              const routeName = "saveRoute" + name;
-              const savedRoute = JSON.stringify({
-                name: routeName,
-                distance: distance.total,
-                conversion: distance.conversion,
-                description,
-                markers: markers.value,
-                directions: directions.value,
-                route: route.value,
-              });
-              AsyncStorage.setItem(routeName, savedRoute);
-              navigation.navigate("Map");
-            }}>
-            <View>
-              <Text style={styles.saveText}>Save Route</Text>
-            </View>
-          </TouchableNativeFeedback>
-        </View>
       </View>
+      <TouchableNativeFeedback
+        onPress={() =>{
+          if (!name) {
+            Alert.alert("Please enter a name for your route!");
+            return;
+          }
+          const routeName = "saveRoute" + name;
+          const savedRoute = JSON.stringify({
+            name: routeName,
+            distance: distance.total,
+            conversion: distance.conversion,
+            description,
+            markers: markers.value,
+            directions: directions.value,
+            route: route.value,
+          });
+          AsyncStorage.setItem(routeName, savedRoute);
+          navigation.navigate("Map");
+        }}
+        >
+        <View style={styles.buttonView}>
+          <Icon
+            color={"#b0b2f5"}
+            name={"save"}
+            size={30}
+          />
+        <Text style={{alignSelf: "center", fontSize: 18, padding: 5, paddingBottom: 10, color: "white"}}>
+            Save Route
+          </Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   )
 }
@@ -83,38 +97,34 @@ export default SaveScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "stretch",
+    backgroundColor: "#b0f5f2"
   },
 
-  emptySpace: {
-    flex: .1,
-    flexDirection: "row",
+  mapStyle: {
+    height: 250,
     backgroundColor: "deepskyblue",
-  },
-
-  detailsView: {
-    flex: .25,
     flexDirection: "row",
-  },
-
-  saveOptions: {
-    flex: .65,
+    margin: 10,
+    marginTop: 30,
+    padding: 20,
   },
 
   nameRow: {
-    flex: 0.2,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-evenly",
+    padding: 10,
     paddingLeft: 50,
     paddingRight: 50,
+    marginTop: 15
   },
 
   descriptionRow: {
-    flex: 0.4,
     flexDirection: "row",
     justifyContent: "space-evenly",
     padding: 50,
+    paddingTop: 10,
+    marginTop: 10,
   },
 
   nameInput: {
@@ -129,27 +139,18 @@ const styles = StyleSheet.create({
   },
 
   buttonView: {
-    flex: .2,
-    justifyContent: "center",
+    flexDirection: "row",
     alignSelf: "center",
+    justifyContent: "center",
     backgroundColor: "deepskyblue",
-    borderRadius: 12,
+    height: 60,
+    padding: 10,
   },
 
-  submitButton: {
-    marginRight: 40,
-    marginLeft: 40,
-    marginTop: 10,
-    backgroundColor: "deepskyblue",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#fff"
+  saveButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    alignSelf: "stretch",
   },
-
-  saveText: {
-    color: "white",
-    fontSize: 18,
-  }
-
-
 });

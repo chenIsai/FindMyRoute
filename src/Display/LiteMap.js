9dc2a39991b65ac619757abf06b8a203e7be5445
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-15 01:46:55 -0400

    Moved LiteView to proper folder, split Map part to separate file

---
+++ src/Display/LiteMap.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {View, StyleSheet} from "react-native";
+import MapView, {Marker, Polyline} from "react-native-maps";
+
+const LiteMap = (props) => {
+  let map = null;
+  return (
+    <MapView
+      liteMode
+      style={{flex: 1}}
+      ref={ref => {map = ref;}}
+      showsUserLocation={false}
+      initialRegion={{latitude: 0, longitude: 0, latitudeDelta: 0.0721, longitudeDelta: 0.0421}}
+      onMapReady={() =>{
+          map.fitToCoordinates(props.markers, {
+    edgePadding: { top: 100, right: 100, bottom: 100, left: 100 },
+    animated: true,
+  });
+        }}
+      >
+      {props.markers.map((coordinate) => {
+        const key = coordinate.latitude.toString() + "," + coordinate.longitude.toString();
+        return(<Marker coordinate={coordinate} key={key}/>)
+      })}
+      <Polyline
+        coordinates={props.directions}
+        strokeWidth={6}
+      />
+    </MapView>
+  )
+}
+export default LiteMap;
--- src/Display/LiteView.js
+++ src/Display/LiteView.js
@@ -1,35 +1,15 @@
 import React from "react";
 import {View, Text, Button, StyleSheet} from "react-native";
 import MapView, {Marker, Polyline} from "react-native-maps";
-import {Badge} from "native-base";
+import LiteMap from "./LiteMap";
+
 
 function LiteView(props) {
   let map = null;
   return(
     <View style={styles.container}>
       <View style={styles.map}>
-        <MapView
-          liteMode
-          style={{flex: 1}}
-          ref={ref => {map = ref;}}
-          showsUserLocation={false}
-          initialRegion={{latitude: 0, longitude: 0, latitudeDelta: 0.0721, longitudeDelta: 0.0421}}
-          onMapReady={() =>{
-              map.fitToCoordinates(props.markers, {
-        edgePadding: { top: 100, right: 100, bottom: 100, left: 100 },
-        animated: true,
-      });
-            }}
-          >
-          {props.markers.map((coordinate) => {
-            const key = coordinate.latitude.toString() + "," + coordinate.longitude.toString();
-            return(<Marker coordinate={coordinate} key={key}/>)
-          })}
-          <Polyline
-            coordinates={props.directions}
-            strokeWidth={6}
-          />
-        </MapView>
+        <LiteMap directions={props.directions} markers={props.markers}/>
       </View>
       <View style={styles.details}>
         <View style={{flex: .2, justifyContent: "center", borderBottomWidth: 1}}>

865a4488879bafff087653a12cefb6f3af2e9b25
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-20 17:54:07 -0400

    Switched MapDisplay to a functional component

--- src/Map/MapDisplay.js
+++ src/Map/MapDisplay.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 import {
   StyleSheet,
   View,
   Button,
-  Alert
+  Alert,
+  Animated
 } from "react-native";
 import Picker from "@react-native-community/picker";
 import Icon from "react-native-vector-icons/Ionicons";
@@ -15,41 +16,13 @@ import {decode} from "@mapbox/polyline";
 
 const apiKey = "";
 
-export default class MapDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      marginBottom : 1,
-      calculated: this.props.route.value.length,
-      totalMarkers: this.props.markers.value.length,
-    }
-  }
+const MapDisplay = (props) => {
+  const mapRef = useRef(null);
+  const [marginBottom, updateMargin] = useState(1);
+  const [calculated, updateCalculated] = useState(props.route.value.length);
+  const [totalMarkers, updateTotalMarkers] = useState(props.markers.value.length);
 
-  componentDidMount() {
-    this.findPosition(true);
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.markers.value !== prevProps.markers.value) {
-      this.getDirections();
-    }
-  }
-
-  _onMapReady = () => this.setState({marginBottom: 0});
-
-
-  onLongPress = (e) => {
-    if (this.state.totalMarkers <= 9) {
-      this.state.totalMarkers++;
-      var coordinate = e.nativeEvent.coordinate
-      this.props.markers.updateMarkers(coordinate);
-      this.getDirections();
-    } else {
-      Alert.alert("Max number of markers reached!");
-    }
-  }
-
-  findPosition = (animate) => {
+  const findPosition = (animate) => {
     Geolocation.getCurrentPosition(
       position => {
         var lat = position.coords["latitude"];
@@ -62,7 +35,8 @@ export default class MapDisplay extends React.Component {
           longitudeDelta: 0.0421,
         };
         if (animate) {
-          this.map.animateToRegion(newRegion);
+          console.log(mapRef);
+          mapRef.current.animateToRegion(newRegion);
         }
       },
       error => {
@@ -73,39 +47,47 @@ export default class MapDisplay extends React.Component {
     );
   };
 
+  const _onMapReady = () => updateMargin(0);
 
-  mergeCoords = (startLoc, endLoc) => {
+  const onLongPress = (e) => {
+    if (totalMarkers <= 9) {
+      updateTotalMarkers(totalMarkers+1);
+      const coordinate = e.nativeEvent.coordinate;
+      props.markers.updateMarkers(coordinate);
+    } else {
+      Alert.alert("Max number of markers reached!");
+    }
+  }
+
+  const mergeCoords = (startLoc, endLoc) => {
     const start = `${startLoc.latitude},${startLoc.longitude}`;
     const end = `${endLoc.latitude},${endLoc.longitude}`;
     return {start, end};
   }
 
-
-  async getDirections() {
-    if (this.state.calculated >= this.props.markers.value.length-1) {
+  const getDirections = async () => {
+    if (calculated >= props.markers.value.length-1) {
       return;
     }
-    var merged = this.mergeCoords(this.props.markers.value[this.state.calculated], this.props.markers.value[this.state.calculated+1]);
+    var merged = mergeCoords(props.markers.value[calculated], props.markers.value[calculated+1]);
     var start = merged.start;
     var end = merged.end;
 
     try {
       const raw = await fetch(`https://maps.googleapis.com/maps/api/directions/json?mode=walking&origin=${start}&destination=${end}&key=${apiKey}`);
       const response = await raw.json();
-      this.props.route.updateRoute(response.routes[0].overview_polyline.points);
-      this.props.distance.updateDistance(response.routes[0].legs[0].distance.value);
-      const updated = this.decodeResponse(response);
-      this.setState({directions: updated.directions, calculated: updated.calculated}, () => {
-        this.props.directions.updateDirections(updated.directions);
-        this.getDirections();
-      });
+      props.route.updateRoute(response.routes[0].overview_polyline.points);
+      props.distance.updateDistance(response.routes[0].legs[0].distance.value);
+      const updated = decodeResponse(response);
+      updateCalculated(updated.calculated);
+      props.directions.updateDirections(updated.directions);
     }
     catch(error) {
       console.log(error);
     }
   }
 
-  decodeResponse = (response) => {
+  const decodeResponse = (response) => {
     const points = decode(response.routes[0].overview_polyline.points);
     const directions = points.map(point => {
       return {
@@ -113,60 +95,66 @@ export default class MapDisplay extends React.Component {
         longitude: point[1]
       }
     });
-    const newDirections = this.props.directions.value.concat(directions);
-    const calculated = this.state.calculated+1;
-    return {directions: newDirections, calculated};
+    const newDirections = props.directions.value.concat(directions);
+    const totalCalculated = calculated+1
+    return {directions: newDirections, calculated: totalCalculated};
   }
 
-
-  clearMarkers = () => {
-    this.props.markers.clearMarkers();
-    this.setState({calculated: 0, directions: [], totalMarkers: 0});
-    this.props.distance.clearDistance();
-    this.props.route.clearRoute();
-    this.props.directions.updateDirections([]);
-    this.forceUpdate();
+  const clearMarkers = () => {
+    props.markers.clearMarkers();
+    updateCalculated(0);
+    updateTotalMarkers(0);
+    props.distance.clearDistance();
+    props.route.clearRoute();
+    props.directions.updateDirections([]);
   }
 
+  useEffect(() => {
+    findPosition(true);
+  }, [])
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <MapView
-          ref={(map) => {this.map = map;}}
-          showsUserLocation={true}
-          style={{flex: 1, marginBottom: this.state.marginBottom}}
-          onMapReady={this._onMapReady}
-          onLongPress={this.onLongPress}
-          >
-          {this.props.markers.value.map((coordinate, index) => {
-            return (<Marker coordinate={coordinate} key={index}/>)
-          })}
-          <Polyline
-            coordinates={this.props.directions.value}
-            strokeWidth={6}
-          />
-        </MapView>
-        <View style={styles.menuView}>
-          <Icon
-            name={"md-menu"}
-            size={30}
-            onPress={() => this.props.navigation.openDrawer()}
-          />
-        </View>
-        <View style={(styles.iconStyles)}>
-          <Icon
-            name={"add-outline"}
-            size={30}
-            onPress={() => console.log("Open tray")} />
-        </View>
+  useEffect(() => {
+    getDirections();
+  }, [props.markers.value])
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        ref={mapRef}
+        showsUserLocation={true}
+        style={{flex: 1, marginBottom: marginBottom}}
+        onMapReady={_onMapReady}
+        onLongPress={onLongPress}
+        >
+        {props.markers.value.map((coordinate, index) => {
+          return (<Marker coordinate={coordinate} key={index}/>)
+        })}
+        <Polyline
+          coordinates={props.directions.value}
+          strokeWidth={6}
+        />
+      </MapView>
+      <View style={styles.menuView}>
+        <Icon
+          name={"md-menu"}
+          size={30}
+          onPress={() => props.navigation.openDrawer()}
+        />
+      </View>
+      <View style={(styles.iconStyles)}>
+        <Icon
+          name={"add-outline"}
+          size={30}
+          onPress={() => console.log("Open tray")} />
+      </View>
+      <View style={{opacity: 1}}>
         <View style={[styles.iconStyles, {bottom: 75}]}>
           <Icon
             name={"save-outline"}
             size={30}
             onPress={() => {
-              if (this.props.markers.value.length > 1 && this.props.directions.value.length) {
-                this.props.navigation.push("SaveScreen")
+              if (props.markers.value.length > 1 && props.directions.value.length) {
+                props.navigation.push("SaveScreen")
               } else {
                 Alert.alert("Invalid Route Selected!");
               }
@@ -176,12 +164,14 @@ export default class MapDisplay extends React.Component {
           <Icon
             name={"trash-outline"}
             size={30}
-            onPress={() => this.clearMarkers()} />
+            onPress={() => clearMarkers()} />
         </View>
       </View>
-    );
-  }
+    </View>
+  );
 }
+
+export default MapDisplay;
 
 const styles = StyleSheet.create({
   container: {

9d9ad3671fae6b4690190a3229f90d09f77ad5d1
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-21 00:05:08 -0400

    Added modal popup to confirm button presses

--- src/Display/Settings.js
+++ src/Display/Settings.js
@@ -1,72 +1,58 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {View, Text, TouchableNativeFeedback, StyleSheet} from "react-native";
+
 import UnitContext from "../Context/UnitContext";
 import DistanceContext from "../Context/UnitContext";
 import AuthContext from "../Context/AuthContext";
+import UserContext from "../Context/UserContext";
+
 import {Picker} from "@react-native-community/picker";
 import Icon from "react-native-vector-icons/Ionicons";
 import links from "../Authentication/link";
 import Header from "./Header";
+import Modal from "react-native-modal";
+import {createStackNavigator} from "@react-navigation/stack";
+// import Edit from "./Edit";
 
-const Settings = (props) => {
+const Stack = createStackNavigator();
+
+const Settings = () => {
   return (
-    <View style={styles.container}>
-      <Header navigation={props.navigation} header={"Profile"}/>
-      <Profile />
-      <Details />
-      <Options />
-    </View>
+    <Stack.Navigator screenOptions = {{headerShown: false}}>
+      <Stack.Screen name={"Profile"} component={MainScreen}/>
+    </Stack.Navigator>
+          // <Stack.Screen name={"Edit"} component={Edit} />
   )
 }
 
-const Profile = () => {
-  return (
-    <View style={styles.profile}>
-      <Icon
-        style={styles.iconStyle}
-        name={"person"}
-        size={80}/>
-      <View style={styles.textStack}>
-        <Text style={{fontWeight: "bold"}}>Name</Text>
-        <Text style={{color: "grey"}}>Username</Text>
-      </View>
-    </View>
-  )
-}
+const MainScreen = (props) => {
+  const [modalVisible, setVisible] = useState(false);
+  const [buttonID, updateID] = useState(0);
+    const tokens = useContext(AuthContext);
+  const logoutText = "Are you sure you want to log out?";
+  const deleteText = "Are you sure you want to delete ALL routes?";
 
-const Details = () => {
-  const unit = useContext(UnitContext);
-  const distance = useContext(DistanceContext);
-  return (
-    <View style={styles.detailsView}>
-      <View style={{alignSelf: "baseline", alignItems: "center"}}>
-        <Text style={{fontWeight: "bold"}}>Saved Routes</Text>
-        <Text style={{color: "dimgrey"}}>69</Text>
-      </View>
-      <View style={{alignSelf: "baseline", alignItems: "center"}}>
-        <Text style={{fontWeight: "bold"}}>Distance Ran</Text>
-        <Text style={{color: "dimgrey"}}>{distance.value}{unit.value}</Text>
-      </View>
-    </View>
-  )
-}
+  const openModal = (id) => {
+    updateID(id);
+    setVisible(true);
+  }
 
-const Options = () => {
-  return (
-    <View style={styles.optionsView}>
-      <Text style={{padding: 5, paddingLeft: 10, fontWeight: "bold"}}>Options</Text>
-      <View>
-        <OptionsRow name={"update"} />
-        <UnitPicker />
-        <OptionsRow name={"clearSaved"} hide={true} />
-        <Logout />
-      </View>
-    </View>
-  )
-}
+  const logout = () => {
+    fetch(links.logout, {
+      method: "DELETE",
+      body: JSON.stringify({
+        token: tokens.refreshToken}),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then((response) => {
+      tokens.updateAccess("");
+      tokens.updateRefresh("");
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
 
-const OptionsRow = (props) => {
-  const tokens = useContext(AuthContext);
   const clearRoutes = () => {
     fetch(links.deleteALL, {
       method: "DELETE",
@@ -78,15 +64,109 @@ const OptionsRow = (props) => {
       console.log(error);
     });
   }
+
+  return (
+    <View style={styles.container}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        isVisible={modalVisible}
+        onRequestClose={() => {
+          setVisible(false);
+        }}
+        onBackdropPress={() => {
+          setVisible(false);
+        }}
+        backdropOpacity={0}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalView}>
+            <Text style={{fontSize: 18, padding: 10}}>{buttonID === 1 ? logoutText : deleteText }</Text>
+            <View style = {{flexDirection: "row",}}>
+              <TouchableNativeFeedback
+                onPress={() => setVisible(false)}>
+                <View style={styles.cancelButton}>
+                  <Text style={{fontWeight: "bold", color: "grey"}}>Cancel</Text>
+                </View>
+              </TouchableNativeFeedback>
+              <TouchableNativeFeedback
+                onPress={() => {
+                  setVisible(false);
+                  buttonID === 1 ? logout() : clearRoutes();
+                }}>
+                <View style={styles.negativeButton}>
+                  <Text style={{color: "white"}}>{buttonID === 1 ? "Log me out!" : "Delete ALL Routes"}</Text>
+                </View>
+              </TouchableNativeFeedback>
+            </View>
+          </View>
+        </View>
+      </Modal>
+      <Header navigation={props.navigation} header={"Profile"}/>
+      <Profile />
+      <Details />
+      <Options showModal={(id) => openModal(id)}/>
+    </View>
+  )
+}
+
+const Profile = () => {
+  const user = useContext(UserContext);
+  return (
+    <View style={styles.profile}>
+      <Icon
+        style={styles.iconStyle}
+        name={"person"}
+        size={80}/>
+      <View style={styles.textStack}>
+        <Text style={{fontWeight: "bold"}}>{user.value.name}</Text>
+        <Text style={{color: "grey"}}>{user.value.username}</Text>
+      </View>
+    </View>
+  )
+}
+
+const Details = () => {
+  const unit = useContext(UnitContext);
+  const user = useContext(UserContext);
+  return (
+    <View style={styles.detailsView}>
+      <View style={{alignSelf: "baseline", alignItems: "center"}}>
+        <Text style={{fontWeight: "bold"}}>Saved Routes</Text>
+        <Text style={{color: "dimgrey"}}>{user.value.routes}</Text>
+      </View>
+      <View style={{alignSelf: "baseline", alignItems: "center"}}>
+        <Text style={{fontWeight: "bold"}}>Distance Ran</Text>
+        <Text style={{color: "dimgrey"}}>{user.value.distance} {unit.value}</Text>
+      </View>
+    </View>
+  )
+}
+
+const Options = (props) => {
+  return (
+    <View style={styles.optionsView}>
+      <Text style={{padding: 5, paddingLeft: 10, fontWeight: "bold"}}>Options</Text>
+      <View>
+        <OptionsRow name={"update"} />
+        <UnitPicker />
+        <OptionsRow name={"clearSaved"} hide={true} showModal={props.showModal}/>
+        <Logout showModal={props.showModal}/>
+      </View>
+    </View>
+  )
+}
+
+const OptionsRow = (props) => {
   const callbacks = {
-    "clearSaved": clearRoutes,
-    "update": clearRoutes,
+    "clearSaved": () => props.showModal(0),
+    "update": () => console.log("Update"),
   };
   const names = {
     clearSaved: "Clear Saved Routes",
     update: "Update Profile",
   };
-  if (props.hide) {
+  if (!props.hide) {
     return (
       <TouchableNativeFeedback style={{alignSelf: "strecth"}}onPress={callbacks[props.name]}>
         <View style={styles.optionsRow}>
@@ -111,26 +191,9 @@ const OptionsRow = (props) => {
   )
 }
 
-const Logout = () => {
-  const tokens = useContext(AuthContext);
-  const logout = () => {
-    console.log("Logging Out");
-    fetch(links.logout, {
-      method: "DELETE",
-      body: JSON.stringify({
-        token: tokens.refreshToken}),
-      headers: {
-        "Content-Type": "application/json",
-      }
-    }).then((response) => {
-        tokens.updateRefresh("");
-        tokens.updateAccess("");
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
+const Logout = (props) => {
   return (
-    <TouchableNativeFeedback onPress={logout}>
+    <TouchableNativeFeedback onPress={() => props.showModal(1)}>
       <View style={styles.optionsRow}>
         <Text style={{alignSelf: "center", color: "red"}}>Log Out</Text>
       </View>
@@ -221,7 +284,45 @@ const styles = StyleSheet.create({
     borderWidth: 0.3,
     backgroundColor: "white",
     borderColor: "silver",
-  }
+  },
+
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  modalView: {
+    width: 350,
+    backgroundColor: "white",
+    borderWidth: 0.7,
+    elevation: 6,
+    overflow: "hidden"
+  },
+
+  cancelButton: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 8,
+    marginTop: 0,
+    padding: 15,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "grey"
+  },
+
+  negativeButton: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 8,
+    marginTop: 0,
+    padding: 15,
+    backgroundColor: "#ED4337",
+    borderRadius: 5,
+  },
 
 })
 

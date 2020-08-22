e5951c9dc099fdf1bdd3a177632c0793b8920fa0
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-22 00:19:14 -0400

    Basic interval clock

--- src/Display/Summary.js
+++ src/Display/Summary.js
@@ -1,5 +1,5 @@
-import React, {useContext} from "react";
-import {View, Text, TouchableNativeFeedback} from "react-native";
+import React, {useContext, useState, useRef} from "react";
+import {View, Text, TouchableNativeFeedback, StyleSheet} from "react-native";
 
 import DistanceContext from "../Context/DistanceContext";
 import UnitContext from "../Context/UnitContext";
@@ -11,14 +11,79 @@ const Summary = (props) => {
   const distance = useContext(DistanceContext);
   const unit = useContext(UnitContext);
   const showDistance = unit.value === "m" ? distance.total : (
-    unit.value === "km" ? distance.total/1000 : Math.round((distance.total/1609 + Number.EPSILON) * 100)/100);
+    unit.value === "km" ? Math.round((distance.total/1000 + Number.EPSILON) * 100)/100 : Math.round((distance.total/1609 + Number.EPSILON) * 100)/100);
   return (
-    <View>
+    <View style={styles.container}>
       <Header navigation={props.navigation} header={"Display"}/>
-      <Text>{showDistance}</Text>
-      <Text>{unit.value}</Text>
+      <View style={{flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+        <Text>{showDistance} {unit.value}</Text>
+      </View>
+      <CurrentRun />
+    </View>
+  )
+}
+
+const CurrentRun = () => {
+  const [time, updateTime] = useState(0);
+  const [isRunning, setRunning] = useState(false);
+  const timerRef = useRef();
+
+  const startTimer = () => {
+    const interval = setInterval(onTick, 1000);
+    setRunning(true);
+    timerRef.current = interval;
+  }
+  const onTick = () => {
+    updateTime(prevTime => prevTime + 1);
+  }
+
+  const stopTimer = () => {
+    clearInterval(timerRef.current);
+    setRunning(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.runDetails}>
+        <View style={{alignSelf: "baseline", alignItems: "center"}}>
+          <Text style={{fontWeight: "bold"}}>Current Distance Ran</Text>
+          <Text style={{color: "dimgrey"}}>Ye</Text>
+        </View>
+        <View style={{alignSelf: "baseline", alignItems: "center"}}>
+          <Text style={{fontWeight: "bold"}}>Average Speed</Text>
+          <Text style={{color: "dimgrey"}}>Ye</Text>
+        </View>
+      </View>
+      <TouchableNativeFeedback
+        onPress={() => isRunning ? stopTimer() : startTimer()}
+        background={TouchableNativeFeedback.Ripple("grey")}
+        >
+        <View style={styles.startStopButton}>
+          <Text style={{color: "white", fontSize: 24,}}>{isRunning ? "Stop run" : "Start Run"}</Text>
+        </View>
+      </TouchableNativeFeedback>
+      <Text style={{alignSelf: "center", marginTop: 50, fontSize: 20}}> {Math.floor(time/60)}:{time % 60 > 9 ? time % 60 : "0" + time % 60}</Text>
     </View>
   )
 }
 
 export default Summary;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "lime"
+  },
+
+  startStopButton: {
+    alignSelf: "center",
+    backgroundColor: "lightblue",
+    padding: 10,
+  },
+
+  runDetails: {
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+})

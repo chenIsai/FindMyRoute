5e9ad8dfb946b8e85a7fa66311e6d8331eac8558
Isaiah Chen <58130815+chenIsai@users.noreply.github.com>
2020-08-25 19:30:16 -0400

    Added sanity checking before rendering additional map features

--- src/Display/LiteMap.js
+++ src/Display/LiteMap.js
@@ -12,26 +12,30 @@ const LiteMap = (props) => {
       showsUserLocation={false}
       initialRegion={{latitude: 0, longitude: 0, latitudeDelta: 0.0721, longitudeDelta: 0.0421}}
       onMapReady={() => {
-        if (props.markers.length) {
+        if (props.markers && props.markers.length) {
           map.fitToCoordinates(props.markers, {
             edgePadding: { top: 100, right: 100, bottom: 100, left: 100 },
             animated: false,
           });
-        } else {
+        } else if (props.directions && props.directions.length) {
           map.fitToCoordinates(props.directions, {
             edgePadding: { top: 100, right: 100, bottom: 100, left: 100}, animated: false,
           });
         }
         }}
       >
-      {props.markers.map((coordinate) => {
-        const key = coordinate.latitude.toString() + "," + coordinate.longitude.toString();
-        return(<Marker coordinate={coordinate} key={key}/>)
-      })}
-      <Polyline
-        coordinates={props.directions}
-        strokeWidth={4}
-      />
+      {props.markers ? (
+        props.markers.map((coordinate) => {
+          const key = coordinate.latitude.toString() + "," + coordinate.longitude.toString();
+          return(<Marker coordinate={coordinate} key={key}/>)
+        })) : null
+      }
+      {props.directions.length ? (
+        <Polyline
+          coordinates={props.directions}
+          strokeWidth={4}
+        />
+      ) : null}
     </MapView>
   )
 }
